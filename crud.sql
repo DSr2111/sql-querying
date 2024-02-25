39723026198d45feb76489a428d09a17aff90037
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-24 23:10:59 -0500

    first 3 questions

---
+++ crud.sql
@@ -0,0 +1,12 @@
+---question 1--
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('chair', 44.00, f);
+
+--q2--
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('stool', 25.99, t);
+
+--q3--
+INSERT INTO products (name, price, can_be_returned)
+VALUES ('table', 124.00, f);
+
