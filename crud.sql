53303b02e522d5faf6971c6890133a4f4cadf637
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-24 23:40:50 -0500

    all 14 q's done

--- crud.sql
+++ crud.sql
@@ -33,3 +33,13 @@ SELECT * FROM products WHERE price < 44.00;
 SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
 
 --q11--
+UPDATE products SET price = price - 20;
+
+--q12--
+DELETE FROM products WHERE price < 25;
+
+--q13--
+UPDATE products SET price = price + 20;
+
+--q14--
+UPDATE products SET can_be_returned = 't';
